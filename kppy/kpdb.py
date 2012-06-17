f2e62a97a83deaba541c991097aa03787143e6f6
Karsten-Kai König <kkoenig@posteo.de>
2012-06-17 14:36:59 +0200

    Now able to write KeePassX-files

--- kppy/kpdb.py
+++ kppy/kpdb.py
@@ -33,6 +33,7 @@ class StdGroup(object):
     - id_ is the group id
     - title is the group title
     - image is the image number used in KeePassX
+    - level is needed to create the group tree
     - parent is the previous group
     - children is a list of all following groups
     - entries is a list of all entries of the group
@@ -43,6 +44,7 @@ class StdGroup(object):
         self.id_ = None
         self.title = None
         self.image = None
+        self.level = None
         self.parent = None
         self.children = []
         self.entries = []
@@ -127,8 +129,8 @@ class KPDB(object):
         self._root_group = StdGroup()
         self._group_order = []
         self._entry_order = []
-        self._header = b''
-        self._crypted_content = b''
+        self._unsupported_g_fields = []
+        self._unsupported_e_fields = []
         self._signature1 = 0x9AA2D903
         self._signature2 = 0xB54BFB65
         self._enc_flag = 0
@@ -177,47 +179,53 @@ class KPDB(object):
             handler.close()
         
         # The header is 124 bytes long, the rest is content
-        self._header = buf[:124]
-        self._crypted_content = buf[124:]
+        header = buf[:124]
+        crypted_content = buf[124:]
         del buf
         
         # The header holds two signatures
-        if not (struct.unpack('<I', self._header[:4])[0] == 0x9AA2D903 and
-                struct.unpack('<I', self._header[4:8])[0] == 0xB54BFB65):
+        if not (struct.unpack('<I', header[:4])[0] == 0x9AA2D903 and
+                struct.unpack('<I', header[4:8])[0] == 0xB54BFB65):
             raise KPError('Wrong signatures!')
+            del crypted_content
+            del header
             return False
 
         # Unpack the header
-        self._enc_flag = struct.unpack('<I', self._header[8:12])[0]
-        self._version = struct.unpack('<I', self._header[12:16])[0]
-        self._final_randomseed = struct.unpack('<16s', self._header[16:32])[0]
-        self._enc_iv = struct.unpack('<16s', self._header[32:48])[0]
-        self._num_groups= struct.unpack('<I', self._header[48:52])[0]
-        self._num_entries = struct.unpack('<I', self._header[52:56])[0]
-        self._contents_hash = struct.unpack('<32s', self._header[56:88])[0]
-        self._transf_randomseed = struct.unpack('<32s', self._header[88:120])[0]
-        self._key_transf_rounds = struct.unpack('<I', self._header[120:124])[0]
+        self._enc_flag = struct.unpack('<I', header[8:12])[0]
+        self._version = struct.unpack('<I', header[12:16])[0]
+        self._final_randomseed = struct.unpack('<16s', header[16:32])[0]
+        self._enc_iv = struct.unpack('<16s', header[32:48])[0]
+        self._num_groups= struct.unpack('<I', header[48:52])[0]
+        self._num_entries = struct.unpack('<I', header[52:56])[0]
+        self._contents_hash = struct.unpack('<32s', header[56:88])[0]
+        self._transf_randomseed = struct.unpack('<32s', header[88:120])[0]
+        self._key_transf_rounds = struct.unpack('<I', header[120:124])[0]
+        del header
 
         # Check if the database is supported
         if self._version & 0xFFFFFF00 != 0x00030002 & 0xFFFFFF00:
             raise KPError('Unsupported file version!')
+            del crypted_content
             return False
         elif not self._enc_flag & 2:
             raise KPError('Unsupported file encryption!')
+            del crypted_content
             return False
         
         # Create the key that is needed to...
         final_key = self._transform_key()
         # ...decrypt the content
-        decrypted_content = self._cbc_decrypt(final_key)
+        decrypted_content = self._cbc_decrypt(final_key, crypted_content)
         del final_key
-        
+
         # Check if decryption failed
         if (len(decrypted_content) > 2147483446) or \
             (len(decrypted_content) == 0 and self._num_groups > 0):
             raise KPError("Decryption failed!\nThe key is wrong or the file is"
                           " damaged.")
             del decrypted_content
+            del crypted_content
             return False
         
         # (Implement correct comparison of contents hash and final key)
@@ -227,26 +235,28 @@ class KPDB(object):
         levels = []
         cur_group = 0
         group = StdGroup()
-        
+
         while cur_group < self._num_groups:
             # Every group is made up of single fields
             field_type = struct.unpack('<H', decrypted_content[:2])[0]
             decrypted_content = decrypted_content[2:]
             pos += 2
-
+            
             # Check if offset is alright
-            if pos >= len(self._crypted_content)+124:
+            if pos >= len(crypted_content)+124:
                 raise KPError('Unexpected error: Offset is out of range.[G1]')
                 del decrypted_content
+                del crypted_content
                 return False
             
             field_size = struct.unpack('<I', decrypted_content[:4])[0]
             decrypted_content = decrypted_content[4:]
             pos += 4
             
-            if pos >= len(self._crypted_content)+124:
+            if pos >= len(crypted_content)+124:
                 raise KPError('Unexpected error: Offset is out of range.[G2]')
                 del decrypted_content
+                del crypted_content
                 return False
 
             # Finally read out the content
@@ -258,13 +268,20 @@ class KPDB(object):
                 self.groups.append(group)
                 group = StdGroup()
                 cur_group += 1
-
+            
+            # Some fields are currently not implemented
+            if field_type == 0x0003 or field_type == 0x0004 or \
+                field_type == 0x0005 or field_type == 0x0006 or \
+                field_type == 0x0009 or field_type == 0x0000:
+                self._unsupported_g_fields.append(decrypted_content[:field_size])
+    
             decrypted_content = decrypted_content[field_size:]
             pos += field_size
-
-            if pos >= len(self._crypted_content)+124:
+           
+            if pos >= len(crypted_content)+124:
                 raise KPError('Unexpected error: Offset is out of range.[G1]')
                 del decrypted_content
+                del crypted_content
                 return False
 
             #This is needed to write changes
@@ -278,19 +295,21 @@ class KPDB(object):
             field_type = struct.unpack('<H', decrypted_content[:2])[0]
             decrypted_content = decrypted_content[2:]
             pos += 2
-            
-            if pos >= len(self._crypted_content)+124:
+             
+            if pos >= len(crypted_content)+124:
                 raise KPError('Unexpected error: Offset is out of range.[G1]')
                 del decrypted_content
+                del crypted_content
                 return False
             
             field_size = struct.unpack('<I', decrypted_content[:4])[0]
             decrypted_content = decrypted_content[4:]
             pos +=4
             
-            if pos >= len(self._crypted_content)+124:
+            if pos >= len(crypted_content)+124:
                 raise KPError('Unexpected error: Offset is out of range.[G2]')
                 del decrypted_content
+                del crypted_content
                 return False
             
             b_ret = self._read_entry_field(entry, field_type, field_size,
@@ -304,33 +323,105 @@ class KPDB(object):
                 entry = StdEntry()
                 cur_entry += 1
             
+            if field_type == 0x0000 or field_type == 0x0001:
+                self._unsupported_e_fields.append(decrypted_content[:field_size])
+
             decrypted_content = decrypted_content[field_size:]
             pos += field_size
             
-            if pos >= len(self._crypted_content)+124:
+            if pos >= len(crypted_content)+124:
                 raise KPError('Unexpected error: Offset is out of range.[G1]')
                 del decrypted_content
+                del crypted_content
                 return False
 
-            self._entry_order.append( (field_type, field_size) )
+            self._entry_order.append((field_type, field_size))
 
         if not self._create_group_tree(levels):
             raise KPError('Invalid group tree.')
             del decrypted_content
+            del crypted_content
             return false
 
         del decrypted_content
+        del crypted_content
 
-    def save(self):
+    def save(self, filepath = None):
+        """This method saves the database.
+
+        It's possible to parse a data path to an alternative file.
+
+        """
         if self.read_only:
             raise KPError("The database has been opened read-only.")
-            return false
+            return False
         
-        self._num_groups = len(self.groups)
-        self._num_entries = len(self._entries)
+        content = bytearray()
+        pos = 0
+        group = 0
+
+        # First, read out all groups
+        for field_type, field_size in self._group_order:
+            # Get the packed bytes
+            ret_save = self._save_group_field(field_type, field_size, 
+                                                self.groups[group])
+
+            # Catch illegal group fields, maybe the data was wrong manipulated
+            if ret_save is False:
+                raise KPError("Illegal group field found while saving")
+                return False
+
+            # The field type and the size is always in front of the data
+            content += struct.pack('<H', field_type)
+            content += struct.pack('<I', field_size)
+
+            # Write unsupported files
+            if field_type == 0x0000 or field_type == 0x0003 or \
+                field_type == 0x0004 or field_type == 0x0005 or \
+                field_type == 0x0006 or field_type == 0x0009:
+                content += self._unsupported_g_fields[pos]
+                pos += 1
+            elif not field_type == 0xFFFF:
+                content += ret_save
+            # If it's the end of the group, go to the next one
+            elif field_type == 0xFFFF:
+                group += 1
+
+        pos = 0
+        entry = 0
+
+        # Same with entries
+        for field_type, field_size in self._entry_order:
+            ret_save = self._save_entry_field(field_type, field_size,
+                                                self._entries[entry])
+
+            if ret_save is False:
+                raise KPError("Illegal entry field found while saving")
+                return False
+
+            content += struct.pack('<H', field_type)
+            content += struct.pack('<I', field_size)
+
+            if field_type == 0x0000 or field_type == 0x0001:
+                content += self._unsupported_e_fields[pos]
+                pos += 1
+            elif not field_type == 0xFFFF:
+                content += ret_save
+            elif field_type == 0xFFFF:
+                entry += 1
+
+        # Generate new seed and new vector; calculate the new hash
         self._final_randomseed = Random.get_random_bytes(16)
         self._enc_iv = Random.get_random_bytes(16)
-        
+        sha_obj = SHA256.new()
+        sha_obj.update(content)
+        self._contents_hash = sha_obj.digest()
+        del sha_obj
+
+        # Pack the header
+        header = bytearray()
+        header += struct.pack('<I', 0x9AA2D903)
+        header += struct.pack('<I', 0xB54BFB65)
         header += struct.pack('<I', self._enc_flag)
         header += struct.pack('<I', self._version)
         header += struct.pack('<16s', self._final_randomseed)
@@ -341,10 +432,26 @@ class KPDB(object):
         header += struct.pack('<32s', self._transf_randomseed)
         header += struct.pack('<I', self._key_transf_rounds)
 
-        for i in self.groups:
-            for field_type, field_size in self._group_order:
-                content += self._save_group_field(field_type, field_size)
-    
+        # Finally encrypt everything...
+        final_key = self._transform_key()
+
+        encrypted_content = self._cbc_encrypt(content, final_key)
+        del content
+
+        # ...and write it out
+        try:
+            if filepath is None:
+                handler = open(self.filepath, 'wb')
+            else:
+                handler = open(filepath, 'wb')
+            handler.write(header+encrypted_content)
+        except IOError as e:
+            raise KPError("Error while opening or writing the file")
+            handler.close
+            return False
+        finally:
+            handler.close
+
     def _transform_key(self):
         """This method creates the key to decrypt the database"""
 
@@ -367,16 +474,26 @@ class KPDB(object):
         sha_obj.update(self._final_randomseed + hashed_key)
         return sha_obj.digest()
     
-    def _cbc_decrypt(self, final_key):
+    def _cbc_decrypt(self, final_key, crypted_content):
         """This method decrypts the database"""
 
         # Just decrypt the content with the created key
         aes = AES.new(final_key, AES.MODE_CBC, self._enc_iv)
-        decrypted_content = aes.decrypt(self._crypted_content)
+        decrypted_content = aes.decrypt(crypted_content)
         padding = decrypted_content[-1]
         decrypted_content = decrypted_content[:len(decrypted_content)-padding]
         
         return decrypted_content
+
+    def _cbc_encrypt(self, content, final_key):
+        aes = AES.new(final_key, AES.MODE_CBC, self._enc_iv)
+        padding = (16 - len(content) % AES.block_size)
+
+        for i in range(padding):
+            content += chr(padding).encode()
+
+        temp = bytes(content)
+        return aes.encrypt(temp)
 
     def _read_group_field(self, group, levels, field_type, field_size,  
                           decrypted_content):
@@ -416,6 +533,7 @@ class KPDB(object):
             group.image = struct.unpack('<I', decrypted_content[:4])[0]
         elif field_type == 0x0008:
             level = struct.unpack('<H', decrypted_content[:2])[0]
+            group.level = level
             levels.append(level)
         elif field_type == 0x0009:
             # Not implemented by KeePassX but is defined by the original
@@ -490,7 +608,7 @@ class KPDB(object):
         return True
 
     def _get_date(self, decrypted_content):
-        """This method is used to encode the packed dates of entries"""
+        """This method is used to decode the packed dates of entries"""
         
         # Just copied from original KeePassX source
         date_field = struct.unpack('<5B', decrypted_content[:5])
@@ -508,8 +626,24 @@ class KPDB(object):
         s = dw5 & 0x3F
         return datetime(y, mon, d, h, min_, s)
 
+    def _pack_date(self, date):
+        """This method is used to encode dates"""
+        
+        # Just copied from original KeePassX source
+        y, mon, d, h, min_, s = date.timetuple()[:6]
+
+        dw1 = 0x0000FFFF & ((y>>6) & 0x0000003F)
+        dw2 = 0x0000FFFF & ((y & 0x0000003F)<<2 | ((mon>>2) & 0x00000003))
+        dw3 = 0x0000FFFF & (((mon & 0x0000003)<<6) | ((d & 0x0000001F)<<1) \
+                | ((h>>4) & 0x00000001))
+        dw4 = 0x0000FFFF & (((h & 0x0000000F)<<4) | ((min_>>2) & 0x0000000F))
+        dw5 = 0x0000FFFF & (((min_ & 0x00000003)<<6) | (s & 0x0000003F))
+
+        return struct.pack('<5B', dw1, dw2, dw3, dw4, dw5)        
+
     def _create_group_tree(self, levels):
         """This method creates a group tree"""
+
         if levels[0] != 0: return false;
         
         for i in range(len(self.groups)):
@@ -538,5 +672,86 @@ class KPDB(object):
                     self._entries[e].index = 0           
         return True
 
-    def _save_group_field(self, field_type, field_size):
-        pass
+    def _save_group_field(self, field_type, field_size, group):
+        """This method packs a group field"""
+        
+        if field_type == 0x0000:
+            # Ignored (commentar block)
+            pass
+        elif field_type == 0x0001:
+            return struct.pack('<I', group.id_)
+        elif field_type == 0x0002:
+            return (group.title+'\0').encode()
+        elif field_type == 0x0003:
+            # Not implemented by KeePassX but is defined by the original
+            # KeePass standard
+            # Will be implemented in a later release
+            pass
+        elif field_type == 0x0004:
+            # Not implemented by KeePassX but is defined by the original
+            # KeePass standard
+            # Will be implemented in a later release
+            pass
+        elif field_type == 0x0005:
+            # Not implemented by KeePassX but is defined by the original
+            # KeePass standard
+            # Will be implemented in a later release
+            pass
+        elif field_type == 0x0006:
+            # Not implemented by KeePassX but is defined by the original
+            # KeePass standard
+            # Will be implemented in a later release
+            pass
+        elif field_type == 0x0007:
+            return struct.pack('<I', group.image)
+        elif field_type == 0x0008:
+            return struct.pack('<H', group.level)
+        elif field_type == 0x0009:
+            # Not implemented by KeePassX but is defined by the original
+            # KeePass standard
+            # Will be implemented in a later release
+            pass
+        elif field_type == 0xFFFF:
+            pass
+        else:
+            return False
+
+    def _save_entry_field(self, field_type, field_size, entry):
+        """This group packs a entry field"""
+
+        if field_type == 0x0000:
+            # Ignored
+            pass
+        elif field_type == 0x0001:
+            # Later
+            pass
+        elif field_type == 0x0002:
+            return struct.pack('<I', entry.group_id)
+        elif field_type == 0x0003:
+            return struct.pack('<I', entry.image)
+        elif field_type == 0x0004:
+            return  (entry.title+'\0').encode()
+        elif field_type == 0x0005:
+            return (entry.url+'\0').encode()
+        elif field_type == 0x0006:
+            return (entry.username+'\0').encode()
+        elif field_type == 0x0007:
+            return (entry.password+'\0').encode()
+        elif field_type == 0x0008:
+            return (entry.comment+'\0').encode()
+        elif field_type == 0x0009:
+            return self._pack_date(entry.creation)
+        elif field_type == 0x000A:
+            return self._pack_date(entry.last_mod)
+        elif field_type == 0x000B:
+            return self._pack_date(entry.last_access)
+        elif field_type == 0x000C:
+            return self._pack_date(entry.expire)
+        elif field_type == 0x000D:
+            return (entry.binary_desc+'\0').encode()
+        elif field_type == 0x000E:
+            return entry.binary+'\0'.encode()
+        elif field_type == 0xFFFF:
+            pass
+        else:
+            return False
