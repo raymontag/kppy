252df64ebb79ac888609b082bc61eace24c663ab
Karsten-Kai König <kkoenig@posteo.de>
2012-11-10 22:11:25 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup( 
      name = "kppy", 
-     version = "1.3", 
+     version = "1.3.1", 
      author = "Karsten-Kai König", 
      author_email = "kkoenig@posteo.de",
      url = "http://www.nongnu.org/kppy/",
